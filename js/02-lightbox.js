f20a7a68021fd85e59c65faed486e6139802e2b3
Iryna Burlaka <i.am.blinova@gmail.com>
2023-07-23 10:10:47 +0200

    tack-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+  <a
+  class="gallery__link"
+  href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}" />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryCards(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250ms",
+});
